891341fe74f792f87603008d25caddb60a228e56
Denis Cherniy <d.chernyi@storytelling-software.com>
2022-02-25 11:12:48 +0300

    Create empty iOS 14 project

---
+++ chat-test-assignment/chat-test-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat-test-assignment
+//
+//  Created by Denis Cherniy on 25.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
