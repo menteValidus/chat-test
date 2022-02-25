0f4de8b133a5150ca27551fb189687cd57d112e7
Denis Cherniy <d.chernyi@storytelling-software.com>
2022-02-25 11:21:19 +0300

    Get rid of main storyboard

---
+++ chat-test-assignment/chat-test-assignment/App/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  chat-test-assignment
+//
+//  Created by Denis Cherniy on 25.02.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let viewController = ViewController()
+        viewController.view.backgroundColor = .white
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
