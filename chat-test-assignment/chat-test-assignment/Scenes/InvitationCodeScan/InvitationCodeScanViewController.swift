24d83043e019f0e277749503bdeeeabd10e45ced
Denis Cherniy <d.chernyi@storytelling-software.com>
2022-02-26 20:49:30 +0300

    Small refactoring

--- chat-test-assignment/chat-test-assignment/Scenes/InvitationCodeScan/InvitationCodeScanViewController.swift
+++ chat-test-assignment/chat-test-assignment/Scenes/InvitationCodeScan/InvitationCodeScanViewController.swift
@@ -23,52 +23,10 @@ class InvitationCodeScanViewController: UIViewController {
         view.backgroundColor = UIColor.black
         captureSession = AVCaptureSession()
 
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
-        let videoInput: AVCaptureDeviceInput
-
-        do {
-            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {
-            return
-        }
-
-        if (captureSession.canAddInput(videoInput)) {
-            captureSession.addInput(videoInput)
-        } else {
-            failed()
-            return
-        }
-
-        let metadataOutput = AVCaptureMetadataOutput()
-
-        if (captureSession.canAddOutput(metadataOutput)) {
-            captureSession.addOutput(metadataOutput)
-
-            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metadataOutput.metadataObjectTypes = [.qr]
-        } else {
-            failed()
-            return
-        }
-
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = view.layer.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        self.previewLayer = previewLayer
-
+        configureCaptureSession()
+        configureLayers()
+        
         captureSession.startRunning()
-    }
-
-    func failed() {
-        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK",
-                                   style: .default,
-                                   handler: { [weak self] _ in
-            self?.dismiss(animated: true)
-        }))
-        present(ac, animated: true)
-//        captureSession = nil
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -86,7 +44,56 @@ class InvitationCodeScanViewController: UIViewController {
             captureSession.stopRunning()
         }
     }
+    
+    // MARK: - Configuration
+    
+    private func configureLayers() {
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        self.previewLayer = previewLayer
+    }
 
+    private func configureCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            showFailureAlert()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            showFailureAlert()
+            return
+        }
+    }
+
+    private func showFailureAlert() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK",
+                                   style: .default,
+                                   handler: { [weak self] _ in
+            self?.dismiss(animated: true)
+        }))
+        present(ac, animated: true)
+    }
 }
 
 extension InvitationCodeScanViewController: AVCaptureMetadataOutputObjectsDelegate {
